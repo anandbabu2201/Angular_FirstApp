429980295bb0934d68d36ceddbfc78eca805a201
Anand Babu <anandbabu2201@gmail.com>
2017-06-05 23:44:00 +0530

    changed text

--- quickstart-master/src/app/app.component.js
+++ quickstart-master/src/app/app.component.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular 2';
+        this.name = 'Angular 2 my first Application';
     }
     return AppComponent;
 }());
--- quickstart-master/src/app/app.component.ts
+++ quickstart-master/src/app/app.component.ts
@@ -4,4 +4,4 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `<h1>Hello {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular 2'; }
+export class AppComponent  { name = 'Angular 2 my first Application'; }
